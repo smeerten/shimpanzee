e5189cc38f46bea0f7865d281cb0058e15a3e092
Bas van Meerten <smeerten@gmail.com>
2017-02-22 20:57:40 +0100

    The results can of the game can now be displayed

--- shimpanzee.py
+++ shimpanzee.py
@@ -35,6 +35,8 @@ X2_Y2LIM = 10.0
 Z4LIM = 0.25
 Z5LIM = 0.05
 
+NSTEPS = 1000
+
 
 class ShimSim(object):
     
@@ -84,7 +86,7 @@ class ShimSim(object):
         yz += self.yzGame
         x2_y2 += self.x2_y2Game
         z4 += self.z4Game
-        z4 += self.z5Game
+        z5 += self.z5Game
         self.Mfield = z1*self.Z + z2*self.Z**2 + z3*self.Z**3 + z4*self.Z**4 + z5*self.Z**5  
         self.Mfield += x1*self.X + y1*self.Y + xz*self.X*self.Z + xy*self.X*self.Y + yz*self.Y*self.Z + x2_y2*(self.X**2-self.Y**2)
         self.spectrum, tmp = np.histogram(self.Mfield, self.npoints*self.OVERSAMPLE, (-self.sw/2.0, self.sw/2.0), density=True)
@@ -107,17 +109,17 @@ class ShimSim(object):
         self.z5Game = 0.0
 
     def startGame(self):
-        self.z1Game = np.random.uniform(-Z1LIM, Z1LIM, 1)[0]
-        self.z2Game = np.random.uniform(-Z2LIM, Z2LIM, 1)[0]
-        self.z3Game = np.random.uniform(-Z3LIM, Z3LIM, 1)[0]
-        self.x1Game = np.random.uniform(-X1LIM, X1LIM, 1)[0]
-        self.y1Game = np.random.uniform(-Y1LIM, Y1LIM, 1)[0]
-        self.xzGame = np.random.uniform(-XZLIM, XZLIM, 1)[0]
-        self.xyGame = np.random.uniform(-XYLIM, XYLIM, 1)[0]
-        self.yzGame = np.random.uniform(-YZLIM, YZLIM, 1)[0]
-        self.x2_y2Game = np.random.uniform(-X2_Y2LIM, X2_Y2LIM, 1)[0]
-        self.z4Game = np.random.uniform(-Z4LIM, Z4LIM, 1)[0]
-        self.z5Game = np.random.uniform(-Z5LIM, Z5LIM, 1)[0]
+        self.z1Game = np.random.randint(-NSTEPS, NSTEPS+1)*Z1LIM/NSTEPS
+        self.z2Game = np.random.randint(-NSTEPS, NSTEPS+1)*Z2LIM/NSTEPS
+        self.z3Game = np.random.randint(-NSTEPS, NSTEPS+1)*Z3LIM/NSTEPS
+        self.x1Game = np.random.randint(-NSTEPS, NSTEPS+1)*X1LIM/NSTEPS
+        self.y1Game = np.random.randint(-NSTEPS, NSTEPS+1)*Y1LIM/NSTEPS
+        self.xzGame = np.random.randint(-NSTEPS, NSTEPS+1)*XZLIM/NSTEPS
+        self.xyGame = np.random.randint(-NSTEPS, NSTEPS+1)*XYLIM/NSTEPS
+        self.yzGame = np.random.randint(-NSTEPS, NSTEPS+1)*YZLIM/NSTEPS
+        self.x2_y2Game = np.random.randint(-NSTEPS, NSTEPS+1)*X2_Y2LIM/NSTEPS
+        self.z4Game = np.random.randint(-NSTEPS, NSTEPS+1)*Z4LIM/NSTEPS
+        self.z5Game = np.random.randint(-NSTEPS, NSTEPS+1)*Z5LIM/NSTEPS
         
 
 class ShimFrame(QtWidgets.QScrollArea):
@@ -135,128 +137,153 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.fidSurfaceLine.setReadOnly(True)
         self.grid.addWidget(self.fidSurfaceLine, 1, 0)
 
-        self.grid.addWidget(QtWidgets.QLabel("Z:"), 2, 0)
+        self.z1Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.z1Label, 2, 0)
         self.z1 = QtWidgets.QLineEdit(self)
         self.z1.setAlignment(QtCore.Qt.AlignHCenter)
         self.z1.setText("0.0")
-        self.z1.returnPressed.connect(lambda: self.insertVal(self.z1, self.z1Scale, 1000/Z1LIM))
+        self.z1.returnPressed.connect(lambda: self.insertVal(self.z1, self.z1Scale, NSTEPS/Z1LIM))
         self.grid.addWidget(self.z1, 3, 0)
         self.z1Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.z1Scale.setRange(-1000, 1000)
-        self.z1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z1, 1000/Z1LIM))
+        self.z1Scale.setRange(-NSTEPS, NSTEPS)
+        self.z1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z1, NSTEPS/Z1LIM))
         self.grid.addWidget(self.z1Scale, 4, 0)
 
-        self.grid.addWidget(QtWidgets.QLabel("Z2:"), 5, 0)
+        self.z2Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.z2Label, 5, 0)
         self.z2 = QtWidgets.QLineEdit(self)
         self.z2.setAlignment(QtCore.Qt.AlignHCenter)
         self.z2.setText("0.0")
-        self.z2.returnPressed.connect(lambda: self.insertVal(self.z2, self.z2Scale, 1000/Z2LIM))
+        self.z2.returnPressed.connect(lambda: self.insertVal(self.z2, self.z2Scale, NSTEPS/Z2LIM))
         self.grid.addWidget(self.z2, 6, 0)
         self.z2Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.z2Scale.setRange(-1000, 1000)
-        self.z2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z2, 1000/Z2LIM))
+        self.z2Scale.setRange(-NSTEPS, NSTEPS)
+        self.z2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z2, NSTEPS/Z2LIM))
         self.grid.addWidget(self.z2Scale, 7, 0)
 
-        self.grid.addWidget(QtWidgets.QLabel("Z3:"), 8, 0)
+        self.z3Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.z3Label, 8, 0)
         self.z3 = QtWidgets.QLineEdit(self)
         self.z3.setAlignment(QtCore.Qt.AlignHCenter)
         self.z3.setText("0.0")
-        self.z3.returnPressed.connect(lambda: self.insertVal(self.z3, self.z3Scale, 1000/Z3LIM))
+        self.z3.returnPressed.connect(lambda: self.insertVal(self.z3, self.z3Scale, NSTEPS/Z3LIM))
         self.grid.addWidget(self.z3, 9, 0)
         self.z3Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.z3Scale.setRange(-1000, 1000)
-        self.z3Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z3, 1000/Z3LIM))
+        self.z3Scale.setRange(-NSTEPS, NSTEPS)
+        self.z3Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z3, NSTEPS/Z3LIM))
         self.grid.addWidget(self.z3Scale, 10, 0)
-        
-        self.grid.addWidget(QtWidgets.QLabel("X:"), 2, 1)
+
+        self.x1Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.x1Label, 2, 1)
         self.x1 = QtWidgets.QLineEdit(self)
         self.x1.setAlignment(QtCore.Qt.AlignHCenter)
         self.x1.setText("0.0")
-        self.x1.returnPressed.connect(lambda: self.insertVal(self.x1, self.x1Scale, 1000/X1LIM))
+        self.x1.returnPressed.connect(lambda: self.insertVal(self.x1, self.x1Scale, NSTEPS/X1LIM))
         self.grid.addWidget(self.x1, 3, 1)
         self.x1Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.x1Scale.setRange(-1000, 1000)
-        self.x1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x1, 1000/X1LIM))
+        self.x1Scale.setRange(-NSTEPS, NSTEPS)
+        self.x1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x1, NSTEPS/X1LIM))
         self.grid.addWidget(self.x1Scale, 4, 1)
 
-        self.grid.addWidget(QtWidgets.QLabel("Y:"), 5, 1)
+        self.y1Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.y1Label, 5, 1)
         self.y1 = QtWidgets.QLineEdit(self)
         self.y1.setAlignment(QtCore.Qt.AlignHCenter)
         self.y1.setText("0.0")
-        self.y1.returnPressed.connect(lambda: self.insertVal(self.y1, self.y1Scale, 1000/Y1LIM))
+        self.y1.returnPressed.connect(lambda: self.insertVal(self.y1, self.y1Scale, NSTEPS/Y1LIM))
         self.grid.addWidget(self.y1, 6, 1)
         self.y1Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.y1Scale.setRange(-1000, 1000)
-        self.y1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.y1, 1000/Y1LIM))
+        self.y1Scale.setRange(-NSTEPS, NSTEPS)
+        self.y1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.y1, NSTEPS/Y1LIM))
         self.grid.addWidget(self.y1Scale, 7, 1)
 
-        self.grid.addWidget(QtWidgets.QLabel("XZ:"), 2, 2)
+        self.xzLabel = QtWidgets.QLabel()
+        self.grid.addWidget(self.xzLabel, 2, 2)
         self.xz = QtWidgets.QLineEdit(self)
         self.xz.setAlignment(QtCore.Qt.AlignHCenter)
         self.xz.setText("0.0")
-        self.xz.returnPressed.connect(lambda: self.insertVal(self.xz, self.xzScale, 1000/XZLIM))
+        self.xz.returnPressed.connect(lambda: self.insertVal(self.xz, self.xzScale, NSTEPS/XZLIM))
         self.grid.addWidget(self.xz, 3, 2)
         self.xzScale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.xzScale.setRange(-1000, 1000)
-        self.xzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xz, 1000/XZLIM))
+        self.xzScale.setRange(-NSTEPS, NSTEPS)
+        self.xzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xz, NSTEPS/XZLIM))
         self.grid.addWidget(self.xzScale, 4, 2)
 
-        self.grid.addWidget(QtWidgets.QLabel("XY:"), 5, 2)
+        self.xyLabel = QtWidgets.QLabel()
+        self.grid.addWidget(self.xyLabel, 5, 2)
         self.xy = QtWidgets.QLineEdit(self)
         self.xy.setAlignment(QtCore.Qt.AlignHCenter)
         self.xy.setText("0.0")
-        self.xy.returnPressed.connect(lambda: self.insertVal(self.xy, self.xyScale, 1000/XYLIM))
+        self.xy.returnPressed.connect(lambda: self.insertVal(self.xy, self.xyScale, NSTEPS/XYLIM))
         self.grid.addWidget(self.xy, 6, 2)
         self.xyScale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.xyScale.setRange(-1000, 1000)
-        self.xyScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xy, 1000/XYLIM))
+        self.xyScale.setRange(-NSTEPS, NSTEPS)
+        self.xyScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xy, NSTEPS/XYLIM))
         self.grid.addWidget(self.xyScale, 7, 2)
 
-        self.grid.addWidget(QtWidgets.QLabel("YZ:"), 8, 2)
+        self.yzLabel = QtWidgets.QLabel()
+        self.grid.addWidget(self.yzLabel, 8, 2)
         self.yz = QtWidgets.QLineEdit(self)
         self.yz.setAlignment(QtCore.Qt.AlignHCenter)
         self.yz.setText("0.0")
-        self.yz.returnPressed.connect(lambda: self.insertVal(self.yz, self.yzScale, 1000/YZLIM))
+        self.yz.returnPressed.connect(lambda: self.insertVal(self.yz, self.yzScale, NSTEPS/YZLIM))
         self.grid.addWidget(self.yz, 9, 2)
         self.yzScale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.yzScale.setRange(-1000, 1000)
-        self.yzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.yz, 1000/YZLIM))
+        self.yzScale.setRange(-NSTEPS, NSTEPS)
+        self.yzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.yz, NSTEPS/YZLIM))
         self.grid.addWidget(self.yzScale, 10, 2)
-        
-        self.grid.addWidget(QtWidgets.QLabel("X2-Y2:"), 11, 2)
+
+        self.x2_y2Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.x2_y2Label, 11, 2)
         self.x2_y2 = QtWidgets.QLineEdit(self)
         self.x2_y2.setAlignment(QtCore.Qt.AlignHCenter)
         self.x2_y2.setText("0.0")
-        self.x2_y2.returnPressed.connect(lambda: self.insertVal(self.x2_y2, self.x2_y2Scale, 1000/X2_Y2LIM))
+        self.x2_y2.returnPressed.connect(lambda: self.insertVal(self.x2_y2, self.x2_y2Scale, NSTEPS/X2_Y2LIM))
         self.grid.addWidget(self.x2_y2, 12, 2)
         self.x2_y2Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.x2_y2Scale.setRange(-1000, 1000)
-        self.x2_y2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x2_y2, 1000/X2_Y2LIM))
+        self.x2_y2Scale.setRange(-NSTEPS, NSTEPS)
+        self.x2_y2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x2_y2, NSTEPS/X2_Y2LIM))
         self.grid.addWidget(self.x2_y2Scale, 13, 2)
 
-        self.grid.addWidget(QtWidgets.QLabel("z4:"), 11, 0)
+        self.z4Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.z4Label, 11, 0)
         self.z4 = QtWidgets.QLineEdit(self)
         self.z4.setAlignment(QtCore.Qt.AlignHCenter)
         self.z4.setText("0.0")
-        self.z4.returnPressed.connect(lambda: self.insertVal(self.z4, self.z4Scale, 1000/Z4LIM))
+        self.z4.returnPressed.connect(lambda: self.insertVal(self.z4, self.z4Scale, NSTEPS/Z4LIM))
         self.grid.addWidget(self.z4, 12, 0)
         self.z4Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.z4Scale.setRange(-1000, 1000)
-        self.z4Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z4, 1000/Z4LIM))
+        self.z4Scale.setRange(-NSTEPS, NSTEPS)
+        self.z4Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z4, NSTEPS/Z4LIM))
         self.grid.addWidget(self.z4Scale, 13, 0)
 
-        self.grid.addWidget(QtWidgets.QLabel("z5:"), 14, 0)
+        self.z5Label = QtWidgets.QLabel()
+        self.grid.addWidget(self.z5Label, 14, 0)
         self.z5 = QtWidgets.QLineEdit(self)
         self.z5.setAlignment(QtCore.Qt.AlignHCenter)
         self.z5.setText("0.0")
-        self.z5.returnPressed.connect(lambda: self.insertVal(self.z5, self.z5Scale, 1000/Z5LIM))
+        self.z5.returnPressed.connect(lambda: self.insertVal(self.z5, self.z5Scale, NSTEPS/Z5LIM))
         self.grid.addWidget(self.z5, 15, 0)
         self.z5Scale = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.z5Scale.setRange(-1000, 1000)
-        self.z5Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z5, 1000/Z5LIM))
+        self.z5Scale.setRange(-NSTEPS, NSTEPS)
+        self.z5Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z5, NSTEPS/Z5LIM))
         self.grid.addWidget(self.z5Scale, 16, 0)
         QtCore.QTimer.singleShot(100, self.resizeAll)
+        self.setTitleText()
 
+    def setTitleText(self, z1='', z2='', z3='', x1='', y1='', xz='', xy='', yz='', x2_y2='', z4='', z5=''):
+        self.z1Label.setText('Z: ' + str(z1))
+        self.z2Label.setText('Z2: ' + str(z2))
+        self.z3Label.setText('Z3: ' + str(z3))
+        self.x1Label.setText('X: ' + str(x1))
+        self.y1Label.setText('Y: ' + str(y1))
+        self.xzLabel.setText('XZ: ' + str(xz))
+        self.xyLabel.setText('XY: ' + str(xy))
+        self.yzLabel.setText('YZ: ' + str(yz))
+        self.x2_y2Label.setText('X2-Y2: ' + str(x2_y2))
+        self.z4Label.setText('Z4: ' + str(z4))
+        self.z5Label.setText('Z5: ' + str(z5))
+        
     def resizeAll(self):
         self.setMinimumWidth(self.grid.sizeHint().width() + self.verticalScrollBar().sizeHint().width())
         
@@ -355,6 +382,10 @@ class SettingsFrame(QtWidgets.QWidget):
         self.gameButton.stateChanged.connect(self.checkGame)
         grid.addWidget(self.gameButton, 0, 8)
 
+        self.resultsButton = QtWidgets.QCheckBox('Results', self)
+        self.resultsButton.stateChanged.connect(self.results)
+        grid.addWidget(self.resultsButton, 0, 9)
+        
         self.resetButton = QtWidgets.QPushButton('Reset Shims', self)
         self.resetButton.clicked.connect(self.father.resetShims)
         grid.addWidget(self.resetButton, 1, 8)
@@ -401,6 +432,12 @@ class SettingsFrame(QtWidgets.QWidget):
             self.father.startGame()
         else:
             self.father.resetGame()
+        
+    def results(self, val):
+        if val:
+            self.father.shimFrame.setTitleText(-self.shimSim.z1Game, -self.shimSim.z2Game, -self.shimSim.z3Game, -self.shimSim.x1Game, -self.shimSim.y1Game, -self.shimSim.xzGame, -self.shimSim.xyGame, -self.shimSim.yzGame, -self.shimSim.x2_y2Game, -self.shimSim.z4Game, -self.shimSim.z5Game)
+        else:
+            self.father.shimFrame.setTitleText()
 
 
 class ShimPlotFrame(Plot1DFrame):
