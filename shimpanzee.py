7fd61de8d00fbb81d46d37f92a83f56e48f37bc0
bmeerten <smeerten@gmail.com>
2017-11-09 14:56:25 +0100

    The shimpanzee now allows the simulation of spherical samples

--- shimpanzee.py
+++ shimpanzee.py
@@ -159,7 +159,7 @@ def rotMatrix_z(size, angle):
 
 class ShimSim(object):
     
-    def __init__(self, N=100000, dimensions=[2.5,10], angles=[0,0], sw=500.0, npoints=512):
+    def __init__(self, N=100000, dimensions=[2.5,10], angles=[0,0], sw=500.0, npoints=512, sphere=False):
         super(ShimSim, self).__init__()
         self.OVERSAMPLE = 16
         self.N = N
@@ -169,21 +169,31 @@ class ShimSim(object):
         self.phi = angles[1]
         self.sw = sw # sweepwidth [kHz]
         self.npoints = npoints # number of points on the frequency axis
+        self.sphere = sphere
         self.fidSurface = 0.0
         self.fwhm = 0.0
         self.resetGame()
         self.setupGrid()
 
     def setupGrid(self):
-        self.z = np.random.uniform(-self.l/2.0, self.l/2.0, self.N)
-        R = np.random.triangular(0, self.r, self.r, self.N)
-        angle = np.random.uniform(0, 2*np.pi, self.N)
-        self.x = R*np.cos(angle)
-        self.y = R*np.sin(angle)
+        if self.sphere:
+            phi = np.random.uniform(0, 2*np.pi, self.N)
+            costheta = np.random.uniform(-1, 1, self.N)
+            u = np.random.uniform(0, 1, self.N)
+            theta = np.arccos(costheta)
+            R = self.r * np.cbrt(u)
+            self.x = R * np.sin(theta) * np.cos(phi)
+            self.y = R * np.sin(theta) * np.sin(phi)
+            self.z = R * np.cos(theta)
+        else:
+            self.z = np.random.uniform(-self.l/2.0, self.l/2.0, self.N)
+            R = np.random.triangular(0, self.r, self.r, self.N)
+            angle = np.random.uniform(0, 2*np.pi, self.N)
+            self.x = R*np.cos(angle)
+            self.y = R*np.sin(angle)
         self.rotMatrix_1 = np.dot(np.dot(X_90_1, rotMatrix_z(1, self.theta)), np.dot(X90_1, rotMatrix_z(1, self.phi)))
         self.rotMatrix_2 = np.dot(np.dot(X_90_2, rotMatrix_z(2, self.theta)), np.dot(X90_2, rotMatrix_z(2, self.phi)))
         self.rotMatrix_3 = np.dot(np.dot(X_90_3, rotMatrix_z(3, self.theta)), np.dot(X90_3, rotMatrix_z(3, self.phi)))
-
         #Rz = np.array([[np.cos(self.phi), np.sin(self.phi), 0], [-np.sin(self.phi), np.cos(self.phi), 0], [0, 0, 1]])
         #Rx = np.array([[1, 0, 0], [0, np.cos(self.theta), np.sin(self.theta)], [0, -np.sin(self.theta), np.cos(self.theta)]])
         #rotMatrix = np.dot(Rz, Rx)
@@ -196,13 +206,11 @@ class ShimSim(object):
         self.Y = np.sqrt(3/(4*np.pi)) * self.y
         self.X = np.sqrt(3/(4*np.pi)) * self.x
         self.Z = np.sqrt(3/(4*np.pi)) * self.z
-        
         self.XY = 0.5*np.sqrt(15/np.pi) * self.x * self.y
         self.YZ = 0.5*np.sqrt(15/np.pi) * self.y * self.z
         self.Z2 = 0.25*np.sqrt(5/np.pi) * (2*self.z**2 - self.x**2 - self.y**2)
         self.XZ = 0.5*np.sqrt(15/np.pi) * self.x * self.z
         self.X2_Y2 = 0.25*np.sqrt(15/np.pi) * (self.x**2 - self.y**2)
-
         self.Y3 = 0.25*np.sqrt(35/(2*np.pi)) * (3*self.x**2 * self.y - self.y**3)
         self.XYZ = 0.5*np.sqrt(105/np.pi) * self.z * self.x * self.y
         self.YZ2 = 0.25*np.sqrt(21/(2*np.pi)) * self.y * (4*self.z**2 - self.x**2 - self.y**2)
@@ -210,7 +218,6 @@ class ShimSim(object):
         self.XZ2 = 0.25*np.sqrt(21/(2*np.pi)) * self.x * (4*self.z**2 - self.x**2 - self.y**2)
         self.ZX2_ZY2 = 0.25*np.sqrt(105/np.pi) * self.z * (self.x**2 - self.y**2)
         self.X3 = 0.25*np.sqrt(35/(2*np.pi)) * (self.x**3 - 3*self.y**2 * self.x) 
-
         self.Mfield = np.zeros(self.x.shape)
         self.freq = np.linspace(-self.sw/2.0, self.sw/2.0, self.npoints)
         self.lb = np.exp(-((10*np.pi*np.arange(self.npoints*self.OVERSAMPLE)/self.sw)**2) / (4.0 * np.log(2)))/self.N
@@ -222,13 +229,11 @@ class ShimSim(object):
         y1 += self.y1Game
         z1 += self.z1Game
         x1 += self.x1Game
-
         xy += self.xyGame
         yz += self.yzGame
         z2 += self.z2Game
         xz += self.xzGame
         x2_y2 += self.x2_y2Game
-
         y3 += self.y3Game
         xyz += self.xyzGame
         yz2 += self.yz2Game
@@ -236,17 +241,14 @@ class ShimSim(object):
         xz2 += self.xz2Game
         zx2_zy2 += self.zx2_zy2Game
         x3 += self.x3Game
-
         [y1, z1, x1] = np.dot(self.rotMatrix_1, [y1, z1, x1])
         [xy, yz, z2, xz, x2_y2] = np.dot(self.rotMatrix_2, [xy, yz, z2, xz, x2_y2])
         [y3, xyz, yz2, z3, xz2, zx2_zy2, x3] = np.dot(self.rotMatrix_3, [y3, xyz, yz2, z3, xz2, zx2_zy2, x3])
-
         self.Mfield = z1*self.Z + z2*self.Z2 + z3*self.Z3
         if not spinning:
             self.Mfield += x1*self.X + y1*self.Y
             self.Mfield += xz*self.XZ + xy*self.XY + yz*self.YZ + x2_y2*self.X2_Y2
             self.Mfield += xz2*self.XZ2 + yz2*self.YZ2 + zx2_zy2*self.ZX2_ZY2 + xyz*self.XYZ + x3*self.X3 + y3*self.Y3
-        
         self.spectrum, tmp = np.histogram(self.Mfield, self.npoints*self.OVERSAMPLE, (-self.sw/2.0, self.sw/2.0))
         self.spectrum = np.fft.ifft(self.spectrum)*self.lb
         self.fidSurface = (np.sum(np.abs(self.spectrum)))/ (self.lbScale)        
@@ -259,13 +261,11 @@ class ShimSim(object):
         self.y1Game = 0.0
         self.z1Game = 0.0
         self.x1Game = 0.0
-        
         self.xyGame = 0.0
         self.yzGame = 0.0
         self.z2Game = 0.0
         self.xzGame = 0.0
         self.x2_y2Game = 0.0
-
         self.y3Game = 0.0
         self.xyzGame = 0.0
         self.yz2Game = 0.0
@@ -307,21 +307,18 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.father = parent
         content = QtWidgets.QWidget(self)
         self.grid = QtWidgets.QGridLayout(content)
-
         self.grid.addWidget(QtWidgets.QLabel("FID Surface [%]:"), 0, 0)
         self.fidSurfaceLine = QtWidgets.QLineEdit(self)
         self.fidSurfaceLine.setAlignment(QtCore.Qt.AlignHCenter)
         self.fidSurfaceLine.setText("0.0")
         self.fidSurfaceLine.setReadOnly(True)
         self.grid.addWidget(self.fidSurfaceLine, 1, 0)
-        
         self.grid.addWidget(QtWidgets.QLabel("FWHM:"), 0, 1)
         self.fwhmLine = QtWidgets.QLineEdit(self)
         self.fwhmLine.setAlignment(QtCore.Qt.AlignHCenter)
         self.fwhmLine.setText("0.0")
         self.fwhmLine.setReadOnly(True)
         self.grid.addWidget(self.fwhmLine, 1, 1)
-        
         self.z1Label = QtWidgets.QLabel()
         self.grid.addWidget(self.z1Label, 2, 0)
         self.z1 = QtWidgets.QLineEdit(self)
@@ -333,7 +330,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.z1Scale.setRange(-NSTEPS, NSTEPS)
         self.z1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z1, NSTEPS/Z1LIM))
         self.grid.addWidget(self.z1Scale, 4, 0)
-
         self.x1Label = QtWidgets.QLabel()
         self.grid.addWidget(self.x1Label, 5, 0)
         self.x1 = QtWidgets.QLineEdit(self)
@@ -345,7 +341,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.x1Scale.setRange(-NSTEPS, NSTEPS)
         self.x1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x1, NSTEPS/X1LIM))
         self.grid.addWidget(self.x1Scale, 7, 0)
-
         self.y1Label = QtWidgets.QLabel()
         self.grid.addWidget(self.y1Label, 8, 0)
         self.y1 = QtWidgets.QLineEdit(self)
@@ -357,7 +352,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.y1Scale.setRange(-NSTEPS, NSTEPS)
         self.y1Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.y1, NSTEPS/Y1LIM))
         self.grid.addWidget(self.y1Scale, 10, 0)
-
         self.z2Label = QtWidgets.QLabel()
         self.grid.addWidget(self.z2Label, 2, 1)
         self.z2 = QtWidgets.QLineEdit(self)
@@ -369,7 +363,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.z2Scale.setRange(-NSTEPS, NSTEPS)
         self.z2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z2, NSTEPS/Z2LIM))
         self.grid.addWidget(self.z2Scale, 4, 1)
-        
         self.xzLabel = QtWidgets.QLabel()
         self.grid.addWidget(self.xzLabel, 5, 1)
         self.xz = QtWidgets.QLineEdit(self)
@@ -381,7 +374,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.xzScale.setRange(-NSTEPS, NSTEPS)
         self.xzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xz, NSTEPS/XZLIM))
         self.grid.addWidget(self.xzScale, 7, 1)
-
         self.yzLabel = QtWidgets.QLabel()
         self.grid.addWidget(self.yzLabel, 8, 1)
         self.yz = QtWidgets.QLineEdit(self)
@@ -393,7 +385,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.yzScale.setRange(-NSTEPS, NSTEPS)
         self.yzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.yz, NSTEPS/YZLIM))
         self.grid.addWidget(self.yzScale, 10, 1)
-
         self.x2_y2Label = QtWidgets.QLabel()
         self.grid.addWidget(self.x2_y2Label, 11, 1)
         self.x2_y2 = QtWidgets.QLineEdit(self)
@@ -405,7 +396,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.x2_y2Scale.setRange(-NSTEPS, NSTEPS)
         self.x2_y2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x2_y2, NSTEPS/X2_Y2LIM))
         self.grid.addWidget(self.x2_y2Scale, 13, 1)
-
         self.xyLabel = QtWidgets.QLabel()
         self.grid.addWidget(self.xyLabel, 14, 1)
         self.xy = QtWidgets.QLineEdit(self)
@@ -417,7 +407,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.xyScale.setRange(-NSTEPS, NSTEPS)
         self.xyScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xy, NSTEPS/XYLIM))
         self.grid.addWidget(self.xyScale, 16, 1)
-        
         self.z3Label = QtWidgets.QLabel()
         self.grid.addWidget(self.z3Label, 2, 2)
         self.z3 = QtWidgets.QLineEdit(self)
@@ -429,7 +418,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.z3Scale.setRange(-NSTEPS, NSTEPS)
         self.z3Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.z3, NSTEPS/Z3LIM))
         self.grid.addWidget(self.z3Scale, 4, 2)
-
         self.xz2Label = QtWidgets.QLabel()
         self.grid.addWidget(self.xz2Label, 5, 2)
         self.xz2 = QtWidgets.QLineEdit(self)
@@ -441,7 +429,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.xz2Scale.setRange(-NSTEPS, NSTEPS)
         self.xz2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.xz2, NSTEPS/XZ2LIM))
         self.grid.addWidget(self.xz2Scale, 7, 2)
-
         self.yz2Label = QtWidgets.QLabel()
         self.grid.addWidget(self.yz2Label, 8, 2)
         self.yz2 = QtWidgets.QLineEdit(self)
@@ -453,7 +440,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.yz2Scale.setRange(-NSTEPS, NSTEPS)
         self.yz2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.yz2, NSTEPS/YZ2LIM))
         self.grid.addWidget(self.yz2Scale, 10, 2)
-        
         self.zx2_zy2Label = QtWidgets.QLabel()
         self.grid.addWidget(self.zx2_zy2Label, 11, 2)
         self.zx2_zy2 = QtWidgets.QLineEdit(self)
@@ -465,7 +451,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.zx2_zy2Scale.setRange(-NSTEPS, NSTEPS)
         self.zx2_zy2Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.zx2_zy2, NSTEPS/ZX2_ZY2LIM))
         self.grid.addWidget(self.zx2_zy2Scale, 13, 2)
-         
         self.xyzLabel = QtWidgets.QLabel()
         self.grid.addWidget(self.xyzLabel, 14, 2)
         self.xyz = QtWidgets.QLineEdit(self)
@@ -477,7 +462,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.xyzScale.setRange(-NSTEPS, NSTEPS)
         self.xyzScale.valueChanged.connect(lambda inp: self.setVal(inp, self.xyz, NSTEPS/XYZLIM))
         self.grid.addWidget(self.xyzScale, 16, 2)
-
         self.x3Label = QtWidgets.QLabel()
         self.grid.addWidget(self.x3Label, 17, 2)
         self.x3 = QtWidgets.QLineEdit(self)
@@ -489,7 +473,6 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.x3Scale.setRange(-NSTEPS, NSTEPS)
         self.x3Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.x3, NSTEPS/X3LIM))
         self.grid.addWidget(self.x3Scale, 19, 2)
-
         self.y3Label = QtWidgets.QLabel()
         self.grid.addWidget(self.y3Label, 20, 2)
         self.y3 = QtWidgets.QLineEdit(self)
@@ -501,9 +484,7 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.y3Scale.setRange(-NSTEPS, NSTEPS)
         self.y3Scale.valueChanged.connect(lambda inp: self.setVal(inp, self.y3, NSTEPS/Y3LIM))
         self.grid.addWidget(self.y3Scale, 22, 2)
-        
         #Make widget lists
-         
         self.lineEditsFirst = [self.y1 , self.x1 , self.z1]
         self.lineEditsSecond = [self.z2 , self.xz, self.yz , self.xy, self.x2_y2]
         self.lineEditsThird = [self.y3 , self.xyz , self.yz2 , self.z3 ,
@@ -513,20 +494,15 @@ class ShimFrame(QtWidgets.QScrollArea):
                          self.yz , self.xy, self.x2_y2,
                          self.y3 , self.xyz , self.yz2 ,
                           self.xz2 , self.xz2 , self.zx2_zy2 , self.x3]
-                          
         self.sliderFirst = [self.y1Scale , self.x1Scale , self.z1Scale]
         self.sliderSecond = [self.z2Scale , self.xzScale, self.yzScale , self.xyScale, self.x2_y2Scale]
         self.sliderThird = [self.y3Scale , self.xyzScale , self.yz2Scale , self.z3Scale ,
                           self.xz2Scale , self.xz2Scale , self.zx2_zy2Scale , self.x3Scale]
-
         self.sliderZ = [self.z1Scale , self.z2Scale, self.z3Scale]
         self.sliderNonZ = [self.y1Scale , self.x1Scale , self.xzScale, 
                          self.yzScale , self.xyScale, self.x2_y2Scale,
                          self.y3Scale , self.xyzScale , self.yz2Scale ,
                           self.xz2Scale , self.xz2Scale , self.zx2_zy2Scale , self.x3Scale]
-        
-        
-        
         QtCore.QTimer.singleShot(100, self.resizeAll)
         self.setTitleText()
 
@@ -583,8 +559,14 @@ class ShimFrame(QtWidgets.QScrollArea):
         self.father.sim(z1=z1, x1=x1, y1=y1, xy=xy, yz=yz, z2=z2, xz=xz, x2_y2=x2_y2, y3=y3, xyz=xyz, yz2=yz2, z3=z3, xz2=xz2, zx2_zy2=zx2_zy2, x3=x3)
 
     def resetShims(self):
-        lineedits = [self.y1, self.z1, self.x1, self.xz, self.yz, self.z2, self.xz, self.x2_y2, self.y3, self.xyz, self.yz2, self.z3, self.xz2, self.zx2_zy2, self.x3]
-        scale = [self.y1Scale, self.z1Scale, self.x1Scale, self.xyScale, self.yzScale, self.z2Scale, self.xzScale, self.x2_y2Scale, self.y3Scale, self.xyzScale, self.yz2Scale, self.z3Scale, self.xz2Scale, self.zx2_zy2Scale, self.x3Scale]
+        lineedits = [self.y1, self.z1, self.x1, self.xy,
+                     self.yz, self.z2, self.xz, self.x2_y2,
+                     self.y3, self.xyz, self.yz2, self.z3,
+                     self.xz2, self.zx2_zy2, self.x3]
+        scale = [self.y1Scale, self.z1Scale, self.x1Scale, self.xyScale,
+                 self.yzScale, self.z2Scale, self.xzScale, self.x2_y2Scale,
+                 self.y3Scale, self.xyzScale, self.yz2Scale, self.z3Scale,
+                 self.xz2Scale, self.zx2_zy2Scale, self.x3Scale]
         for i in range(len(lineedits)):
             lineedits[i].setText("0.0")
             scale[i].blockSignals(True)
@@ -624,8 +606,9 @@ class SettingsFrame(QtWidgets.QWidget):
         self.spinningButton = QtWidgets.QCheckBox('Spinning', self)
         self.spinningButton.stateChanged.connect(self.father.shimFrame.sim)
         grid.addWidget(self.spinningButton, 3, 0)
-
-        
+        self.sphereButton = QtWidgets.QCheckBox('Sphere', self)
+        self.sphereButton.stateChanged.connect(self.setSphere)
+        grid.addWidget(self.sphereButton, 4, 0)
         grid.addWidget(QtWidgets.QLabel("Rotation:"), 0, 2)
         grid.addWidget(QtWidgets.QLabel("Theta [degrees]:"), 1, 2)
         self.theta = QtWidgets.QLineEdit(self)
@@ -639,21 +622,18 @@ class SettingsFrame(QtWidgets.QWidget):
         self.phi.setText(str(self.shimSim.phi/np.pi*180.0))
         self.phi.returnPressed.connect(self.setAngles)
         grid.addWidget(self.phi, 2, 3)
-        
         grid.addWidget(QtWidgets.QLabel("# points:"), 1, 4)
         self.nsamples = QtWidgets.QLineEdit(self)
         self.nsamples.setAlignment(QtCore.Qt.AlignHCenter)
         self.nsamples.setText(str(self.shimSim.N))
         self.nsamples.returnPressed.connect(self.setPoints)
         grid.addWidget(self.nsamples, 1, 5)
-
         grid.addWidget(QtWidgets.QLabel("Sweepwidth:"), 1, 6)
         self.sw = QtWidgets.QLineEdit(self)
         self.sw.setAlignment(QtCore.Qt.AlignHCenter)
         self.sw.setText(str(self.shimSim.sw))
         self.sw.returnPressed.connect(self.setSw)
         grid.addWidget(self.sw, 1, 7)
-        
         grid.addWidget(QtWidgets.QLabel("Game:"), 0, 8)
         self.game1Button = QtWidgets.QPushButton('1st order', self)
         self.game1Button.clicked.connect(lambda: self.father.startGame(1))
@@ -670,15 +650,12 @@ class SettingsFrame(QtWidgets.QWidget):
         self.gameStopButton = QtWidgets.QPushButton('Stop', self)
         self.gameStopButton.clicked.connect(self.father.resetGame)
         grid.addWidget(self.gameStopButton, 5, 8)
-        
         self.resultsButton = QtWidgets.QCheckBox('Results', self)
         self.resultsButton.stateChanged.connect(self.results)
         grid.addWidget(self.resultsButton, 0, 9)
-        
         self.resetButton = QtWidgets.QPushButton('Reset Shims', self)
         self.resetButton.clicked.connect(self.father.resetShims)
         grid.addWidget(self.resetButton, 1, 9)
-        
         grid.setColumnStretch(10, 1)
         grid.setRowStretch(10, 1)
 
@@ -709,6 +686,11 @@ class SettingsFrame(QtWidgets.QWidget):
         self.shimSim.setupGrid()
         self.father.shimFrame.sim()
 
+    def setSphere(self):
+        self.shimSim.sphere = self.sphereButton.isChecked()
+        self.shimSim.setupGrid()
+        self.father.shimFrame.sim()
+        
     def setSw(self):
         sw = safeEval(self.sw.text())
         if sw is not None:
@@ -718,7 +700,11 @@ class SettingsFrame(QtWidgets.QWidget):
         
     def results(self, val):
         if val:
-            self.father.shimFrame.setTitleText(-self.shimSim.y1Game, -self.shimSim.z1Game, -self.shimSim.x1Game, -self.shimSim.xyGame, -self.shimSim.yzGame, -self.shimSim.z2Game, -self.shimSim.xzGame, -self.shimSim.x2_y2Game, -self.shimSim.y3Game, -self.shimSim.xyzGame, -self.shimSim.yz2Game, -self.shimSim.z3Game, -self.shimSim.xz2Game, -self.shimSim.zx2_zy2Game, -self.shimSim.x3Game)
+            self.father.shimFrame.setTitleText(-self.shimSim.y1Game, -self.shimSim.z1Game, -self.shimSim.x1Game,
+                                               -self.shimSim.xyGame, -self.shimSim.yzGame, -self.shimSim.z2Game,
+                                               -self.shimSim.xzGame, -self.shimSim.x2_y2Game, -self.shimSim.y3Game,
+                                               -self.shimSim.xyzGame, -self.shimSim.yz2Game, -self.shimSim.z3Game,
+                                               -self.shimSim.xz2Game, -self.shimSim.zx2_zy2Game, -self.shimSim.x3Game)
         else:
             self.father.shimFrame.setTitleText()
 
@@ -783,7 +769,6 @@ class MainProgram(QtWidgets.QMainWindow):
         self.savedatAct.setToolTip('Export as text')
         self.quitAct = self.filemenu.addAction('&Quit', self.fileQuit, QtGui.QKeySequence.Quit)
         self.quitAct.setToolTip('Close Shimpanzee')
-
         self.fig = Figure()
         self.canvas = FigureCanvas(self.fig)
         self.ax = self.fig.gca()
@@ -807,9 +792,6 @@ class MainProgram(QtWidgets.QMainWindow):
 
     def startGame(self, *args, **kwargs):
         self.shimSim.startGame(*args, **kwargs)
-       
-        
-        
         if len(args) > 0:   
             if args[0] == 1:
                 for widget in self.shimFrame.lineEditsFirst + self.shimFrame.sliderFirst:
@@ -829,11 +811,6 @@ class MainProgram(QtWidgets.QMainWindow):
                 widget.setDisabled(False)
             for widget in self.shimFrame.lineEditsNonZ + self.shimFrame.sliderNonZ:
                 widget.setDisabled(True)
-
-        
-        
-        
-        
         self.resetShims()
         self.shimFrame.sim()
 
